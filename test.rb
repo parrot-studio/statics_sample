998c086f36af5b2afaeb26e9eb82b56b3e94f107
parrot_studio <parrot.studio.dev@gmail.com>
2010-01-14 00:09:49 +0900

    add sample test

---
+++ test.rb
@@ -0,0 +1,16 @@
+require 'standard_deviation_analyzer'
+require 'order_statistic_analyzer'
+
+list = [1,2,3,4,5,6,7,8,9,10]
+sda = StandardDeviationAnalyzer.new(list)
+osa = OrderStatisticAnalyzer.new(list)
+
+p sda.avg
+p sda.div
+
+p osa.median
+p osa.iqr
+p osa.q1
+p osa.q3
+
+
